27d5907bfbede430c8dc6f980e5571ed5fb0f7a2
Klodiana Mazrekaj <klodianamazrekaj20@gmail.com>
2024-11-19 14:00:33 +0100

    customized the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive" size="sm">Click Me</Button>
-      <p className="text-red-500">Klodiana</p>
+    <div className="flex gap-4">
+      <Input />
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
